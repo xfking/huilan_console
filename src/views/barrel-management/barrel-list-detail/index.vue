1b065f76c519ad59fcdba749e3f5fcd38ce0c25a
xfking <643314606@qq.com>
2024-08-18 20:47:35 +0800

    更新代码

--- src/views/barrel-management/barrel-list-detail/index.vue
+++ src/views/barrel-management/barrel-list-detail/index.vue
@@ -1,4 +1,168 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref, onMounted } from "vue";
+import { useRouter } from "vue-router";
+import ContentBox from "@/components/ContentBox.vue";
+import ComponentsUploadImg from "@/components/ComponentsUploadImg.vue";
+
+const router = useRouter();
+
+/** 初始数据 */
+const dataSet = ref({
+  title: "",
+  path: "",
+  desciption: "",
+  state: 1,
+  content: "",
+  img: "",
+});
+
+/** 提交 */
+const onSubmit = () => {
+  console.log("======", dataSet.value);
+};
+
+/** 返回 */
+const onBack = () => {
+  router.go(-1);
+};
+</script>
 <template>
-  <div></div>
+  <div class="content">
+    <!-- 基础信息 -->
+    <ContentBox title="基础信息">
+      <el-form
+        :inline="true"
+        :model="dataSet"
+        class="demo-form-inline"
+        label-width="150"
+        label-suffix="："
+      >
+        <el-row :gutter="20">
+          <el-col :xs="24" :sm="12" :lg="12" :xl="12">
+            <el-form-item label="单桶编号">
+              <el-input
+                v-model="dataSet.title"
+                placeholder="请输入"
+                readonly
+                clearable
+              />
+            </el-form-item>
+          </el-col>
+          <el-col :xs="24" :sm="12" :lg="12" :xl="12">
+            <el-form-item label="单桶名称">
+              <el-input
+                v-model="dataSet.title"
+                placeholder="请输入"
+                clearable
+              />
+            </el-form-item>
+          </el-col>
+          <el-col :xs="24" :sm="12" :lg="12" :xl="12">
+            <el-form-item label="风味">
+              <el-input
+                v-model="dataSet.title"
+                placeholder="请输入"
+                clearable
+              />
+            </el-form-item>
+          </el-col>
+          <el-col :xs="24" :sm="12" :lg="12" :xl="12">
+            <el-form-item label="净含量">
+              <el-input
+                v-model="dataSet.title"
+                placeholder="请输入"
+                clearable
+              />
+            </el-form-item>
+          </el-col>
+          <el-col :xs="24" :sm="12" :lg="12" :xl="12">
+            <el-form-item label="原料">
+              <el-input
+                v-model="dataSet.title"
+                placeholder="请输入"
+                clearable
+              />
+            </el-form-item>
+          </el-col>
+          <el-col :xs="24" :sm="12" :lg="12" :xl="12">
+            <el-form-item label="产品标签">
+              <el-input
+                v-model="dataSet.title"
+                placeholder="请输入"
+                clearable
+              />
+            </el-form-item>
+          </el-col>
+          <el-col :xs="24" :sm="12" :lg="12" :xl="12">
+            <el-form-item label="发酵方式">
+              <el-input
+                v-model="dataSet.title"
+                placeholder="请输入"
+                clearable
+              />
+            </el-form-item>
+          </el-col>
+          <el-col :xs="24" :sm="12" :lg="12" :xl="12">
+            <el-form-item label="蒸馏方式">
+              <el-input
+                v-model="dataSet.title"
+                placeholder="请输入"
+                clearable
+              />
+            </el-form-item>
+          </el-col>
+          <el-col :xs="24" :sm="12" :lg="12" :xl="12">
+            <el-form-item label="入桶强度">
+              <el-input
+                v-model="dataSet.title"
+                placeholder="请输入"
+                clearable
+              />
+            </el-form-item>
+          </el-col>
+          <el-col :xs="24" :sm="12" :lg="12" :xl="12">
+            <el-form-item label="入泥煤强度">
+              <el-input
+                v-model="dataSet.title"
+                placeholder="请输入"
+                clearable
+              />
+            </el-form-item>
+          </el-col>
+          <el-col :xs="24" :sm="12" :lg="12" :xl="12">
+            <el-form-item label="品鉴笔记">
+              <el-input
+                v-model="dataSet.desciption"
+                placeholder="请输入"
+                clearable
+              />
+            </el-form-item>
+          </el-col>
+          <el-col :xs="24" :sm="12" :lg="12" :xl="12">
+            <el-form-item label="启用">
+              <el-switch
+                v-model="dataSet.state"
+                class="ml-2"
+                style="
+                  --el-switch-on-color: #13ce66;
+                  --el-switch-off-color: #ff4949;
+                "
+              />
+            </el-form-item>
+          </el-col>
+
+          <el-col :span="24">
+            <el-form-item label="单桶图片">
+              <ComponentsUploadImg :img="dataSet.img" />
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </el-form>
+    </ContentBox>
+
+    <div class="page_footer">
+      <el-button type="primary" size="large" @click="onSubmit">提交</el-button>
+      <el-button size="large" @click="onBack">返回</el-button>
+    </div>
+  </div>
 </template>
